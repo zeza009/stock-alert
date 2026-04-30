b7680aede047480f23e362c613e50a32484f76c8
zeza009 <zezazeza009@gmail.com>
2026-04-30 22:54:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-tv = TvDatafeed()  # ไม่ต้อง login
+import os
+import ta
+import requests
+import config
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from tvDatafeed import TvDatafeed, Interval  # ← เพิ่มบรรทัดนี้
